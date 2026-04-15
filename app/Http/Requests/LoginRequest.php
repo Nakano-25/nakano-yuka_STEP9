1718286e01fa01a76fdfa1a6298e4fe63437b09e
Nakano-25 <nakano.y.0125@gmail.com>
2026-04-16 06:29:39 +0900

    バリデーションをFormRequestに分離（Login/Register）

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -7,11 +7,24 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class LoginRequest extends FormRequest
 {
+    public function authorize(): bool
+    {
+        return true;
+    }
+
     public function rules(): array
     {
         return [
-            'email' => ['required', 'email'],
-            'password' => ['required'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'string', 'max:255'],
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'email' => 'メールアドレス',
+            'password' => 'パスワード',
         ];
     }
 }
